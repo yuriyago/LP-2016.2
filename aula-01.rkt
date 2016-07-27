5afc930b8532697139db15a07121a3dcecafb3ea
Alexandre Rademaker <arademaker@gmail.com>
2016-07-26 21:45:58 -0300

    first commit

---
+++ aula-01.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (teste-soma-! a b)
+  (+ a b))
+
+(+ (* 3 (+ (* 2 4)
+           (+ 3 5)))
+   (+ (- 10 7) 6))
+
+(define size 2)
+
+
+(define (square x)
+  (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
