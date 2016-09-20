0462afba75d3a5c9d6d4465a5b23bf53f189de3d
Gustavo <guscavaliere@gmail.com>
2016-09-20 16:37:52 -0300

    Change file to make the union-set iterative

--- solucao-2.59.rkt
+++ solucao-2.59.rkt
@@ -23,14 +23,14 @@ set
 #|cond:
 1)verificar se algum dos sets é vazio --> output: set não vazio
 2)verificar se o primeiro elemento (car) do set1 está no set2 ---> output: aplicar o unio-set ao cdr do set1, somente
-3)else: cons do car do set1 com o union-set do cdr do set1 e do set2.|#
+3)else: union-set do cdr do set1 com o cons do car do set1 e o set2.|#
 
 
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
         ((element-of-set? (car set1) set2)(union-set (cdr set1) set2))
-        ( cons (car set1)(union-set (cdr set1) set2))))
+        ( union-set (cdr set1)(cons(car set1) set2))))
 
 (define teste1 (list 1 2 3 4 5 6))
 (define teste2 (list 1 2 3 4 5 6 7 8 9 10))
