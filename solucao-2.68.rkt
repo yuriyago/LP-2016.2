8a94c80c3f0cccf7649f604557809f830702145b
Henrique Muniz <hn.muniz.a@gmail.com>
2016-09-27 20:52:25 -0300

    solucao-2.68 (#17)
    solucao-2.68
    
    * Update solucao-2.68.rkt

---
+++ solucao-2.68.rkt
@@ -0,0 +1,75 @@
+#lang racket
+(define (encode-symbol letter tree)
+  (define (iter symbol tree code)
+    (if (leaf? tree)
+        code
+        (if (element-of-tree? symbol (left-branch tree))
+            (iter symbol (left-branch tree) (append code '(0)))
+            (iter symbol (right-branch tree) (append code '(1))))))
+  (iter letter tree '()))
+
+(define (element-of-tree? symbol tree)
+  (define (aux x set)
+    (cond ((null? set) false)
+          ((equal? x (car set)) true)
+          (else (aux x (cdr set)))))
+  (aux symbol (symbols tree)))
+
+
+
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) 
+                (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch 
+                (car bits) 
+                current-branch)))
+          (if (leaf? next-branch)
+              (cons 
+               (symbol-leaf next-branch)
+               (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) 
+                        next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: 
+               CHOOSE-BRANCH" bit))))
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append 
+       (encode-symbol (car message) 
+                      tree)
+       (encode (cdr message) tree))))
