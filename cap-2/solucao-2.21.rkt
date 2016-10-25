93da8e0d95ff5dcf9da4d38d03eab0823880e433
Alexandre Rademaker <arademaker@gmail.com>
2016-10-25 18:54:16 -0200

    correcoes

--- cap-2/solucao-2.21.rkt
+++ cap-2/solucao-2.21.rkt
@@ -1,9 +1,10 @@
 #lang racket
-(define (square-list1 items)
+
+(define (square-list-1 items)
   (if (null? items)
       empty
       (cons (sqr (car items))
             (square-list1 (cdr items)))))
 
-(define (square-list2 items)
+(define (square-list-2 items)
   (map sqr items))
