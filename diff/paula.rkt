4e6155a6c5e8a1938c24113b65a0add25a05bbac
Alexandre Rademaker <arademaker@gmail.com>
2016-11-24 12:36:19 -0200

    formatacao arquivo e rename

--- diff/paula.rkt
+++ diff/paula.rkt
@@ -7,10 +7,11 @@
 (define (distance a b) ;função auxiliar para "difference"
   (string-levenshtein a b))
 (define (difference lst-1 lst-2) ;função principal
-  (let ((p (length lst-1)))
+  (let ((p (length lst-1))
+        (b (length lst-2)))
     (define (auxdiff lst-1 lst-2 out)
       (if (null? lst-1)
-          (make-matrix p p (reverse out))
+          (make-matrix p b (reverse out))
           (begin (for ((i lst-2))
                    (set! out (append (list (distance (car lst-1) i)) out)))
                  (auxdiff (cdr lst-1) lst-2 out))))
